d27131013d66053fc40fd108ab713f6107a89964
pVang8 <pvang@codestack.co>
2022-01-19 13:30:01 -0800

    fetch working

--- script/app.js
+++ script/app.js
@@ -1,3 +1,6 @@
-fetch('api.openweathermap.org/data/2.5/forecast?q=modesto&appid=beb82069579ce783ca0622097e57eb73')
-.then(response => response.text())
+
+
+//fetch for default display has up coming 5 days and 3 hour forecast
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=modesto&appid=beb82069579ce783ca0622097e57eb73')
+.then(response => response.json())
 .then(data => console.log(data))
