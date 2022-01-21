9c7f661912d9740bedbb4e815d8c7a41007a8eae
pVang8 <pvang@codestack.co>
2022-01-21 10:21:54 -0800

    got time

---
+++ script/time.js
@@ -0,0 +1,18 @@
+
+//Got from stack overflow
+function updateTime(){
+    var currentTime = new Date()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    if (minutes < 10){
+        minutes = "0" + minutes
+    }
+    var t_str = hours + ":" + minutes + " ";
+    if(hours > 11){
+        t_str += "PM";
+    } else {
+        t_str += "AM";
+    }
+    document.getElementById('time').textContent = t_str;
+}
+setInterval(updateTime, 1000);
