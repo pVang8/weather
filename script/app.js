387c2e9fe8a2ef287d518711d4e7ad46888e65ad
pVang8 <pvang@codestack.co>
2022-01-20 20:44:50 -0800

    a lot

--- script/app.js
+++ script/app.js
@@ -4,3 +4,8 @@
 fetch('https://api.openweathermap.org/data/2.5/forecast?q=modesto&appid=beb82069579ce783ca0622097e57eb73')
 .then(response => response.json())
 .then(data => console.log(data))
+
+
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat=37.6393&lon=-120.9970&exclude={part}&appid=beb82069579ce783ca0622097e57eb73')
+.then(resp => resp.json())
+.then(data => console.log(data))
